a8b4898d6339f9948f9399583063ab7c5874ae98
padawan <padawan@debianUPEM-2016>
2020-12-16 18:51:59 +0100

    Gestion des erreurs du au malloc

---
+++ includes/err.h
@@ -0,0 +1,14 @@
+#ifndef ERR_FNT_H
+# define ERR_FNT_H
+
+/*
+ * Fonction gérant erreur lié au malloc
+*/
+void malloc_handling();
+
+/* 
+ * Fonction gérant erreur lié au lecture de fichier ou image
+*/ 
+void err_what(int err);
+
+#endif
