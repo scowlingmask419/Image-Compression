60f5080fdba141690003c093846cf0cf343d892c
padawan <padawan@debianUPEM-2016>
2020-12-17 13:06:00 +0100

    Modification commentaire

--- includes/err.h
+++ includes/err.h
@@ -8,6 +8,7 @@ void malloc_handling();
 
 /*
  * Fonction gérant les erreurs lié a la lecture de fichier ou image
+ * Param err correspond au type de l'erreur fichier ou image
 */ 
 void err_what(int err);
 
