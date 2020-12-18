f3d72a36dce4b0c03bd238a36ab28c15e03e2905
padawan <padawan@debianUPEM-2016>
2020-12-18 14:00:53 +0100

    Bibliotheque dont on aura besoin

---
+++ includes/libinclude.h
@@ -0,0 +1,15 @@
+#ifndef LIBINCLUDE_H
+# define LIBINCLUDE_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <string.h>
+# include <dirent.h>
+# include <math.h>
+# include <MLV/MLV_all.h>
+
+#endif
